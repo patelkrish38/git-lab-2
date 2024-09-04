e821d08e637999b0f00eb1d5394242bb64784168
patelkrish38 <kp008723@ohio.edu>
2024-09-04 17:38:39 -0400

    Git Lab Program

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+/*
+ *      File: git-lab-program.cc
+ *      Author: Krish Patel
+ *      Date: 04 Sep 2024
+ *      Description: Add Description
+ */
+
+ #include <iostream>
+ #include <iomanip>
+ #include <cstdlib>
+ using namespace std;
+
+ int main(int argc, char const *argv[]) 
+ {
+     cout << "Hello Git!!" << endl;
+ }
